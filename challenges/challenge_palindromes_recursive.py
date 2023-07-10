d1cf9ca099bd7800c8d99b30e26c056f0abd4d6a
Karina Gonzalez <karinagonzalez99@gmail.com>
2023-07-10 13:44:59 -0300

    Requisito 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,17 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    # Verifica se a string é vazia
+    if len(word) == 0:
+        return False
+
+    # Verifica se os índices se cruzaram (caso base)
+    if low_index > high_index:
+        return True
+
+    # Verifica se os caracteres nos índices baixo e alto são diferentes
+    if word[low_index] != word[high_index]:
+        return False
+
+    # Chamada recursiva da função com os índices atualizados
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
     raise NotImplementedError
